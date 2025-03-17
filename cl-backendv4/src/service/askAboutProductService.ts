b680c0428761cecde84483a5797f97135b347905
Connor Bovino <connor.bovino@gmail.com>
2025-03-17 13:28:28 -0400

    bedrock service

---
+++ cl-backendv4/src/service/askAboutProductService.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { BedrockQueryService } from "./aiQueryClients/bedrockQueryService";
+
+export const AskAboutProductService = async (req?: string) => {
+    try {
+        if (!req) {
+            throw new Error("No request body provided");
+        }
+        const bedrockQueryService = new BedrockQueryService();
+        const result = await bedrockQueryService.rag(req as string);
+        return result;
+    } catch (error) {
+        return error;
+    }
+
+}
